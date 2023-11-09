b89c91a2f24b75f1c6edbdcee16f848443ba6a0d
TheRant-man <55584170+TheRant-man@users.noreply.github.com>
2023-11-09 10:51:59 +0200

    Update moosik.lua

--- Hooks/moosik.lua
+++ Hooks/moosik.lua
@@ -1,3 +1,3 @@
 Hooks:PostHook(MusicManager, "jukebox_default_tracks", "road_rant_default_track", function(self)
-	self:track_attachment_add("heist_road_rant_name", "")
+	self:track_attachment_add("heist_road_rant_name", "Contra")
 end)
